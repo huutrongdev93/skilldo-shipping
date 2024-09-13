40cb40310ee6a5303452be304044ac16c2a8be55
huutrongdev93 <huutrong.it93@gmail.com>
2024-09-13 11:16:02 +0700

    Điều chỉnh model cms 7.1

--- shipping-function.php
+++ shipping-function.php
@@ -1,9 +1,9 @@
 <?php
 class ShippingFee extends \SkillDo\Model\Model {
 
-    static string $table = 'shipping_fee';
+    protected string $table = 'shipping_fee';
 
-    static array $columns = [
+    protected array $columns = [
         'name'      => ['string'],
         'type'      => ['string'],
         'range'     => ['array'],
@@ -11,50 +11,28 @@ class ShippingFee extends \SkillDo\Model\Model {
         'default'   => ['int', 0],
     ];
 
-    static array $rules = [
-        'created' => true,
-        'updated' => true,
-    ];
-
-    static function get($args = []) {
-        $zone = parent::get($args);
-        if (have_posts($zone)) {
-            if(isset($zone->range)) $zone->range = unserialize($zone->range);
-        }
-        return $zone;
-    }
-
-    static function gets($args = []) {
-        $zone = parent::gets($args);
-        if (have_posts($zone)) {
-            foreach ($zone as $value) {
-                if(isset($value->range)) $value->range = unserialize($value->range);
-            }
-        }
-        return $zone;
-    }
-
-    static function deleteById($objectId = 0): array|bool
+    protected static function boot(): void
     {
-        $objectId = (int)Str::clear($objectId);
+        parent::boot();
 
-        if($objectId == 0 ) return false;
+        static::retrieved(function (ShippingFee $shippingFee) {
+            if(Str::isSerialized($shippingFee->range)) {
+                $shippingFee->range = unserialize($shippingFee->range);
+            }
+        });
+    }
 
-        $model = model(self::$table);
-
-        if($model->delete(Qr::set($objectId))) {
-            return [$objectId];
-        }
-
-        return false;
+    static function deleteById($objectId): array|bool
+    {
+        return static::whereKey($objectId)->remove();
     }
 }
 
 class ShippingZone extends \SkillDo\Model\Model {
 
-    static string $table = 'shipping_zones';
+    protected string $table = 'shipping_zones';
 
-    static array $columns = [
+    protected array $columns = [
         'name'              => ['string'],
         'feeId'             => ['int', 0],
         'city'              => ['string'],
@@ -62,37 +40,19 @@ class ShippingZone extends \SkillDo\Model\Model {
         'districtOption'    => ['int', 0],
     ];
 
-    static array $rules = [
-        'created' => true,
-        'updated' => true,
-    ];
-
-    static function get($args = []) {
-        $zone = parent::get($args);
-        if (have_posts($zone)) {
-            if(isset($zone->districts)) $zone->districts = unserialize($zone->districts);
-        }
-        return $zone;
-    }
-
-    static function gets($args = []) {
-        $zone = parent::gets($args);
-        if (have_posts($zone)) {
-            foreach ($zone as $value) {
-                if(isset($value->districts)) $value->districts = unserialize($value->districts);
-            }
-        }
-        return $zone;
-    }
-
-    static function deleteById($objectId = 0): array|bool
+    protected static function boot(): void
     {
-        $objectId = (int)Str::clear($objectId);
-        if($objectId == 0 ) return false;
-        $model = model(self::$table);
-        if($model->delete(Qr::set($objectId))) {
-            return [$objectId];
-        }
-        return false;
+        parent::boot();
+
+        static::retrieved(function (ShippingZone $shippingZone) {
+            if(Str::isSerialized($shippingZone->districts)) {
+                $shippingZone->districts = unserialize($shippingZone->districts);
+            }
+        });
+    }
+
+    static function deleteById($objectId): array|bool
+    {
+        return static::whereKey($objectId)->remove();
     }
 }
